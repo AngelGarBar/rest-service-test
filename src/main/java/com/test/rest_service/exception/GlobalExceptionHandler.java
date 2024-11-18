85e699785385a38122955f0cddc067d6baa41768
Angel <angelgarbarrios@gmail.com>
2024-11-18 19:31:02 +0100

    Rest-Service +Manejo de excepciones añadido

---
+++ src/main/java/com/test/rest_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+// src/main/java/com/test/rest_service/exception/GlobalExceptionHandler.java
+package com.test.rest_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                "An unexpected error occurred",
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
